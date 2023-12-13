d967d1d9da1fc796973b97e86c37a90efb47330d
Richard Balmer <balmer.richard@gmail.com>
2023-12-13 10:44:42 +0000

    Add code to calculate least common multiple of cycles

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -1,3 +1,5 @@
+import math
+
 filename = "2023/day8/input.txt"
 
 with open(filename, "r") as file:
@@ -19,3 +21,23 @@ while current_node != "ZZZ":
         current_node = node_mappings[current_node][0]
 
 print(hops)
+
+current_nodes = [key for key in node_mappings.keys() if key.endswith("A")]
+cycles = []
+
+for current_node in current_nodes:
+    hops = 0
+    local_instructions = instructions.copy()
+    while current_node[2] != "Z":
+        instruction = local_instructions.pop(0)
+        local_instructions.append(instruction)
+        hops += 1
+
+        if instruction == "R":
+            current_node = node_mappings[current_node][1]
+        elif instruction == "L":
+            current_node = node_mappings[current_node][0]
+
+    cycles.append(hops)
+
+print(math.lcm(*cycles))
